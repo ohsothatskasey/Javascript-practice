71bef5537523e74a380f47fc8664f6be05ec3769
ohsothatskasey <kasey.tenezaca@stu.vertexacademies.org>
2024-10-15 14:08:25 -0400

    Add files via upload

---
+++ Kasey_Tenezaca_JS_01.js
@@ -0,0 +1,16 @@
+// This is a comment.  The // tells the computer to ignore this line.
+// Developers use comments to leave helpful notes to themselves and others.
+
+// Add your code below and click the 'Run code' button to see if it works!
+
+console.log('Hello, World!'); 
+
+console.log("¡Hola, Mundo!")
+
+
+
+console.log("Hello");
+console.log("World")
+
+
+console.log("Output:\n The computer,\"Hello world!\"")
