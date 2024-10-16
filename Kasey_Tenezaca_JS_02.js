45487044a3e97c1e560a116b2abbc097569a7e3c
ohsothatskasey <kasey.tenezaca@stu.vertexacademies.org>
2024-10-16 14:08:23 -0400

    Add files via upload

---
+++ Kasey_Tenezaca_JS_02.js
@@ -0,0 +1,18 @@
+// Example:             // Expected Output
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(4+9 ) //13
+console.log(3**8 ) //6561
+console.log(25/4 ) //6.25 
+console.log(4+5*9 ) //49
+console.log(-2*2.5 ) //-5
+
+console.log(!true); 	
+console.log(!!false); 
+
+console.log(true || true); 	
+console.log(false || true); 	
+
+console.log(true || true); 	
+console.log(false || true); 	
