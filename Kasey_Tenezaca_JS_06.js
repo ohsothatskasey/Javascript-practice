035492409a43e50b21f42a77ff143592574372e8
ohsothatskasey <kasey.tenezaca@stu.vertexacademies.org>
2024-10-23 14:16:42 -0400

    Add files via upload

---
+++ Kasey_Tenezaca_JS_06.js
@@ -0,0 +1,57 @@
+// #1 Click Run to test the function below. Examine the output.
+// Fix the function so that the output is as expected.
+
+function midPoint(num1, num2){ // This function finds the midpoint between two numbers.
+  let total = num2 + num1;
+  
+  let middle = total / 2;
+  return middle
+}
+
+
+// Test                         // Expected Output
+console.log( midPoint(0, 2) );  // 2
+console.log( midPoint(4, 10) ); // 7
+
+
+
+// #2 Complete the function below so that it takes three numbers as arguments
+// and returns the sum of the first two arguments, subtracted by the third argument.
+
+function addThenSubtract(num1, num2, num3){
+  let total = num1 + num2 - num3;
+  let middle = total / 6
+  return num1, num2 , num3
+}
+
+//Test (remove '//' to run these tests) // Expected Output
+console.log( addThenSubtract(3, 3, 6) );  // 0
+console.log( addThenSubtract(5, 8, 7) );  // 6
+
+
+
+// #3 Write a function divideByThree(num) that returns the passed in number argument divided by three.
+
+function divideByThree(num) {
+  // your code here...
+}
+
+
+// Test (remove '//' to run these tests)     // Expected Output
+// console.log(divideByThree(9));                //  3.0
+// console.log(divideByThree(15));               //  5.0
+// console.log(divideByThree(98));               //  32.666666666666664
+
+
+
+
+
+// #4 Write a function averageOfFour(num1, num2, num3, num4) that takes in four numbers.
+// The function should return the average of all of the numbers.
+
+
+// Test (remove '//' to run these tests)     // Expected Output
+// console.log(averageOfFour(10, 10, 15, 5)); //  10
+// console.log(averageOfFour(3, 10, 11, 4));  //  7
+// console.log(averageOfFour(1, 2, 3, 4));    //  2.5
+
